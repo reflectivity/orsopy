33c2eb9af20359953724552d3f65ddb04651be87
Andrew McCluskey <andrew.mccluskey@ess.eu>
2021-07-02 13:28:33 +0200

    flake8

--- orsopy/fileio/__init__.py
+++ orsopy/fileio/__init__.py
@@ -1,3 +1,4 @@
-from orsopy.fileio.base import ValueScalar, ValueRange, ValueVector, Comment, Person, Column
+from orsopy.fileio.base import (ValueScalar, ValueRange, ValueVector, Comment,
+                                Person, Column)
 
 OSRO_VERSION = 0.1
