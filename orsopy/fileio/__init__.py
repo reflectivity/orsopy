eff0d3d10ed4cb8d2468ac20f01a1b3f1c7baa6e
Andrew McCluskey <andrew.mccluskey@ess.eu>
2021-07-07 14:23:29 +0200

    update init

--- orsopy/fileio/__init__.py
+++ orsopy/fileio/__init__.py
@@ -1,5 +1,5 @@
 from orsopy.fileio.base import (ValueScalar, ValueRange, ValueVector, Comment,
-                                Person, Column)
+                                Person, Column, File)
 from orsopy.fileio.reduction import Software, Reduction
 
 OSRO_VERSION = 0.1
