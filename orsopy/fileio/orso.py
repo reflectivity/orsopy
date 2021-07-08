76b0c4a1f6059465a08ca5ec5bec1242b3609ab2
Andrew McCluskey <andrew.mccluskey@ess.eu>
2021-07-08 12:00:54 +0200

    add _orso_optionals

--- orsopy/fileio/orso.py
+++ orsopy/fileio/orso.py
@@ -22,6 +22,7 @@ class Orso(Header):
     reduction: Reduction
     column_description: List[Column]
     data_set: int
+    _orso_optionals = []
 
 
 def make_empty():
@@ -33,7 +34,8 @@ def make_empty():
     :return: Uninformative object
     :rtype: orsopy.fileio.orso.Orso
     """
-    _data_source = DataSource(None, Experiment(None, None, None, None), Sample(None))
+    _data_source = DataSource(None, Experiment(None, None, None, None),
+                              Sample(None))
     _measurement = Measurement(InstrumentSettings(None, None), None)
     _reduction = Reduction(Software(None, None, None), None, None, None)
     return Orso(_data_source, _measurement, _reduction, None, None)
