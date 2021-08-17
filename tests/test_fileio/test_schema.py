298b831ac1058e2b8f411dd7c387deabd087f65a
Andrew Nelson <andyfaff@gmail.com>
2021-08-17 15:56:47 +1000

    PEP8

--- tests/test_fileio/test_schema.py
+++ tests/test_fileio/test_schema.py
@@ -13,11 +13,15 @@ class TestSchema:
         with open(schema_pth, "r") as f:
             schema = json.load(f)
 
-        dct_list, data = _read_header_data(os.path.join("tests", "test_example.ort"))
+        dct_list, data = _read_header_data(
+            os.path.join("tests", "test_example.ort")
+        )
 
         # d contains datetime.datetime objects, which would fail the
         # jsonschema validation, so force those to be strings.
-        modified_dct_list = [json.loads(json.dumps(dct, default=str)) for dct in dct_list]
+        modified_dct_list = [
+            json.loads(json.dumps(dct, default=str)) for dct in dct_list
+        ]
         for dct in modified_dct_list:
             jsonschema.validate(dct, schema)
 
