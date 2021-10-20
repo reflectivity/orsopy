0f8e5977e1e76d4d58f28bd01d643423659fc565
Artur Glavic <artur.glavic@psi.ch>
2021-10-20 13:48:38 +0200

    lint

--- orsopy/fileio/base.py
+++ orsopy/fileio/base.py
@@ -517,7 +517,7 @@ def _read_header_data(file: Union[TextIO, str], validate: bool = False) -> Tuple
         for i, line in enumerate(fi.readlines()):
             if not line.startswith("#"):
                 # ignore empty lines
-                if line.strip()!='':
+                if line.strip() != '':
                     _ds_lines.append(line)
                 continue
 
