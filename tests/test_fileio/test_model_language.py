270454e468a94d0608805f282f1533cf2c3fa496
Artur Glavic <artur.glavic@psi.ch>
2022-03-29 17:10:58 +0200

    minor

--- tests/test_fileio/test_model_language.py
+++ tests/test_fileio/test_model_language.py
@@ -32,11 +32,14 @@ class TestMaterial(unittest.TestCase):
 
     def test_default(self):
         defaults = model_language.ModelParameters(
-            mass_density_unit="g/cm^3", number_density_unit="1/nm^3",
-            sld_unit="1/angstrom^2", magnetic_moment_unit="muB"
+            mass_density_unit="g/cm^3",
+            number_density_unit="1/nm^3",
+            sld_unit="1/angstrom^2",
+            magnetic_moment_unit="muB",
         )
-        m = model_language.Material(formula="Fe2O3", mass_density=7.0, number_density=0.15,
-                                    sld=6.3e-6, magnetic_moment=3.4)
+        m = model_language.Material(
+            formula="Fe2O3", mass_density=7.0, number_density=0.15, sld=6.3e-6, magnetic_moment=3.4
+        )
         m.resolve_defaults(defaults)
 
         assert m.mass_density == Value(7.0, "g/cm^3")
@@ -46,7 +49,7 @@ class TestMaterial(unittest.TestCase):
 
     def test_density_lookup_elements(self):
         # no lookup case
-        m = model_language.Material(sld=Value(4e-6, '1/angstrom^3'))
+        m = model_language.Material(sld=Value(4e-6, "1/angstrom^3"))
         m.generate_density()
         # single element lookup case
         for element in ["Co", "Ni", "Si", "C"]:
@@ -62,8 +65,8 @@ class TestMaterial(unittest.TestCase):
             assert m.comment == "density from average element density"
 
     def test_sld(self):
-        m = model_language.Material(sld=Value(3.4e-6, "1/angstrom^2"))
-        assert m.get_sld() == (3.4e-6 + 0j)
+        m = model_language.Material(sld=ComplexValue(3.4e-6, -2e-6, "1/angstrom^2"))
+        assert m.get_sld() == (3.4e-6 - 2e-6j)
         m = model_language.Material(formula="Si", mass_density=Value(2.33, "g/cm^3"))
         self.assertAlmostEqual(m.get_sld().real, 2.07371e-6, 5)
         self.assertAlmostEqual(m.get_sld().imag, -0.00002e-6, 5)
