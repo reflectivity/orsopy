bc9edd5179d73de05b4851cff5f007eb056c0c7b
Artur Glavic <artur.glavic@psi.ch>
2023-02-22 18:40:30 +0100

    Backport default_factory issue to py36

--- orsopy/fileio/model_language.py
+++ orsopy/fileio/model_language.py
@@ -6,9 +6,9 @@ resolving the model to a simple list of slabs.
 """
 import warnings
 
-from dataclasses import field
 from typing import Any, Dict, List, Optional, Union
 
+from ..dataclasses import field
 from ..utils.chemical_formula import Formula
 from ..utils.density_resolver import DensityResolver
 from .base import ComplexValue, Header, Value, orsodataclass
