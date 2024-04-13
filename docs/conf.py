6de5cd173a6ad0806ce71eb489ffdcb60662fb6d
Brian Benjamin Maranville <brian.maranville@nist.gov>
2024-04-13 16:35:51 -0400

    use ORSO favicon

--- docs/conf.py
+++ docs/conf.py
@@ -99,6 +99,7 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
 
+html_favicon = 'ORSO_Logo_bear.svg'
 
 # -- Options for HTMLHelp output ---------------------------------------
 
