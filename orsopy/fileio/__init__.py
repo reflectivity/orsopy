8700d57c8c1e5cd1e21d7337c4983371cb7c5fba
Artur Glavic <artur.glavic@psi.ch>
2026-03-16 10:21:59 +0100

    Minor

--- orsopy/fileio/__init__.py
+++ orsopy/fileio/__init__.py
@@ -1,8 +1,10 @@
 """
 The role of the :py:mod:`fileio` module is to enable the creation of and parsing from ORSO reduced data files.
-All public classes and functions in the :py:mod:`fileio` module are available directly from :py:mod:`fileio` without needing to specify a particular submodule.
+All public classes and functions in the :py:mod:`fileio` module are available directly from
+:py:mod:`fileio` without needing to specify a particular submodule.
 
-The ORSO file format specification is available `here <https://www.reflectometry.org/advanced_and_expert_level/file_format>`_.
+The ORSO file format specification is available
+`here <https://www.reflectometry.org/advanced_and_expert_level/file_format>`_.
 """
 
 from .base import (Column, ComplexValue, ErrorColumn, File, Header, Person, Value, ValueRange, ValueVector,
